782b0ce5bd87da18704b7718ffaac9854afdebd4
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2024-09-26 11:37:00 +0700

    Add variables

--- DevOps/terraform/main.tf
+++ DevOps/terraform/main.tf
@@ -1,12 +1,11 @@
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
-  location = var.location
 }
 
 resource "azurerm_api_management" "apim_service" {
   name                = "${var.environment}-apim-service"
   location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
   publisher_name      = "Example Publisher"
   publisher_email     = "nhat.thaiquang@asnet.com.vn"
   sku_name            = "Developer_1"
@@ -27,7 +26,7 @@ XML
 
 resource "azurerm_api_management_api" "api" {
   name                = "${var.environment}-api"
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
   api_management_name = azurerm_api_management.apim_service.name
   revision            = "1"
   display_name        = "${var.environment}-api"
@@ -43,7 +42,7 @@ resource "azurerm_api_management_api" "api" {
 
 resource "azurerm_api_management_product" "product" {
   product_id            = "${var.environment}-product"
-  resource_group_name   = azurerm_resource_group.rg.name
+  resource_group_name   = data.azurerm_resource_group.rg.name
   api_management_name   = azurerm_api_management.apim_service.name
   display_name          = "${var.environment}-product"
   subscription_required = true
@@ -54,21 +53,21 @@ resource "azurerm_api_management_product" "product" {
 
 resource "azurerm_api_management_group" "group" {
   name                = "${var.environment}-group"
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
   api_management_name = azurerm_api_management.apim_service.name
   display_name        = "${var.environment}-group"
   description         = "An example group"
 }
 
 resource "azurerm_api_management_product_api" "product_api" {
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
   api_management_name = azurerm_api_management.apim_service.name
   product_id          = azurerm_api_management_product.product.product_id
   api_name            = azurerm_api_management_api.api.name
 }
 
 resource "azurerm_api_management_product_group" "product_group" {
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
   api_management_name = azurerm_api_management.apim_service.name
   product_id          = azurerm_api_management_product.product.product_id
   group_name          = azurerm_api_management_group.group.name
@@ -76,15 +75,15 @@ resource "azurerm_api_management_product_group" "product_group" {
 
 resource "azurerm_application_insights" "example" {
   name                = "example-appinsights"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   application_type    = "other"
 }
 
 resource "azurerm_api_management_logger" "example" {
   name                = "example-logger"
   api_management_name = azurerm_api_management.apim_service.name
-  resource_group_name = azurerm_resource_group.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
   resource_id         = azurerm_application_insights.example.id
 
   application_insights {
@@ -94,7 +93,7 @@ resource "azurerm_api_management_logger" "example" {
 
 resource "azurerm_api_management_api_diagnostic" "apiDiagnostics" {
     for_each = var.apis
-        resource_group_name = azurerm_resource_group.rg.name
+        resource_group_name = data.azurerm_resource_group.rg.name
         api_management_name = azurerm_api_management.apim_service.name
         api_name = each.value.name
         api_management_logger_id = azurerm_api_management_logger.example.id
