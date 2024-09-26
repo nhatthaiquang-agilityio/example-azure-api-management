30660edb04fc291a7d5b53dadae29942f14bffc3
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2024-09-26 13:56:23 +0700

    Update AppInsight

--- DevOps/terraform/main.tf
+++ DevOps/terraform/main.tf
@@ -92,10 +92,51 @@ resource "azurerm_api_management_logger" "example" {
 }
 
 resource "azurerm_api_management_api_diagnostic" "apiDiagnostics" {
-    for_each = var.apis
-        resource_group_name = data.azurerm_resource_group.rg.name
-        api_management_name = azurerm_api_management.apim_service.name
-        api_name = each.value.name
-        api_management_logger_id = azurerm_api_management_logger.example.id
-        identifier = "applicationinsights"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  api_management_name = azurerm_api_management.apim_service.name
+  api_name = azurerm_api_management_api.api.name
+  api_management_logger_id = azurerm_api_management_logger.example.id
+  identifier = "applicationinsights"
+
+  sampling_percentage       = 5.0
+  always_log_errors         = true
+  log_client_ip             = true
+  verbosity                 = "Verbose"
+  http_correlation_protocol = "W3C"
+
+  frontend_request {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "accept",
+      "origin",
+    ]
+  }
+
+  frontend_response {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "content-length",
+      "origin",
+    ]
+  }
+
+  backend_request {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "accept",
+      "origin",
+    ]
+  }
+
+  backend_response {
+    body_bytes = 32
+    headers_to_log = [
+      "content-type",
+      "content-length",
+      "origin",
+    ]
+  }
 }
