1034cebc8a497af9035127210a95ab8a6433e465
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2024-09-30 09:53:09 +0700

    Set Function lvel

--- ExampleAzureFunctions/WelcomeTrigger.cs
+++ ExampleAzureFunctions/WelcomeTrigger.cs
@@ -15,7 +15,7 @@ namespace ExampleAzureFunctions
         }
 
         [Function("Welcome")]
-        public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
+        public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
             return new OkObjectResult("Welcome to Azure Functions!");
